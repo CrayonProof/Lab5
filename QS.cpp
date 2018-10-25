c490be281b8c5396b30c1cfbbb987c583fe0cc04
Jacob Crandall <crandall@cs.byu.edu>
2018-10-25 03:38:53 +0000

    first commit

---
+++ QS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+QS() {}
+~QS() {}
+
+
+void sortAll()
+{
+    
+}
+
+int partition(int left, int right, int pivotIndex)
+{
+    
+}
+
+string getArray() const
+{
+    
+}
+
+int getSize() const
+{
+    return (*array).getSize();
+}
+
+bool addToArray(int value)
+{
+    
+}
+
+bool createArray(int capacity)
+{
+    if(table != 0)
+    {
+        array = new int[capacity];
+    }
+}
+
+void clear()
+{
+    
+}
---
+++ QS.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class QS
+    public QSInterfac:
+{
+public:
+	QS() {}
+	~QS() {}
+
+
+	void sortAll();
+	int partition(int left, int right, int pivotIndex);
+	string getArray() const;
+	int getSize() const;
+	bool addToArray(int value);
+	bool createArray(int capacity);
+	void clear();
+private:
+	int *array;
+};
+	
